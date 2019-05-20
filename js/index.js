e00a4f37b1cd573b23e365f9aea707da45f6e56b
unknown <daniel199257@gmail.com>
2019-05-20 16:50:49 -0300

    Editando arquivos

--- js/index.js
+++ js/index.js
@@ -14,12 +14,10 @@ const botao_abrir = document.querySelector("#abrir-info-menu");
 const botao_fechar = document.querySelector("#fechar-info-menu");
 const expandir_janela = document.querySelectorAll("#info-menu nav ul a li");
 const fechar_janela = document.querySelectorAll(".abrir-menu");
-let auxiliar = 0;
 
 botao_abrir.addEventListener("click", function(){
 
     document.querySelector("#info-menu").classList.add("bounceInDown");
-    console.log( document.querySelector("#info-menu"));
     showEffect(document.querySelector("#info-menu"));
 
 });
@@ -34,8 +32,7 @@ botao_fechar.addEventListener("click", function(){
 for(let i = 0; i <= expandir_janela.length; i++){
 
     expandir_janela[i].onclick = (state) =>{
-
-        auxiliar = 1;
+        
         expandir_janela[i].classList.add("expandir");
         console.log(state, auxiliar);
 
