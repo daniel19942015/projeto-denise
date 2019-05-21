1bf9b3c421bd8d0d08a134eb253cb9ff1645eb1a
unknown <daniel199257@gmail.com>
2019-05-21 13:52:16 -0300

    Atualizando o codigo

--- js/index.js
+++ js/index.js
@@ -14,10 +14,11 @@ const botao_abrir = document.querySelector("#abrir-info-menu");
 const botao_fechar = document.querySelector("#fechar-info-menu");
 const expandir_janela = document.querySelectorAll("#info-menu nav ul a li");
 const fechar_janela = document.querySelectorAll(".abrir-menu");
+let auxiliar = 0;
 
 botao_abrir.addEventListener("click", function(){
 
-    document.querySelector("#info-menu").classList.add("bounceInDown");
+    document.querySelector("#info-menu").classList.add("animation");
     showEffect(document.querySelector("#info-menu"));
 
 });
@@ -27,16 +28,23 @@ botao_fechar.addEventListener("click", function(){
 
     closeEffect(document.querySelector("#info-menu"));
 
+
 });
 
 for(let i = 0; i <= expandir_janela.length; i++){
+            
+            expandir_janela[i].onclick = () =>{ 
+                
+                expandir_janela[i].classList.add("expandir");
+                expandir_janela[i].classList.add("animations");
 
-    expandir_janela[i].onclick = (state) =>{
+                expandir_janela[i].onclick = () =>{
+
+                    expandir_janela[i].classList.remove("expandir");
+                    expandir_janela[i].classList.remove("animations");
+                }   
+            }
         
-        expandir_janela[i].classList.add("expandir");
-        console.log(state, auxiliar);
-
     }
-}
 
 
