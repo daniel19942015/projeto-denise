520fc2659869f79cd10fb71ea26e128b36d98330
Daniel <daniel199257@gmail.com>
2019-05-23 20:34:52 -0300

    Modificando menu responsivo

--- js/index.js
+++ js/index.js
@@ -1,50 +1,53 @@
-let showEffect = element =>{
 
-    element.style.display = "block";
+(function(){
 
-}
+    const botao_abrir = document.querySelector("#abrir-info-menu");
+    const botao_fechar = document.querySelector("#fechar-info-menu");
+    const expandir_janela = document.querySelectorAll("#info-menu nav ul a li");
+   
+    let showEffect = element =>{
 
-let closeEffect = element =>{
+        element.style.display = "block";
 
-    element.style.display = "none";
+    }
 
-}
+    let closeEffect = element =>{
 
-const botao_abrir = document.querySelector("#abrir-info-menu");
-const botao_fechar = document.querySelector("#fechar-info-menu");
-const expandir_janela = document.querySelectorAll("#info-menu nav ul a li");
-const fechar_janela = document.querySelectorAll(".abrir-menu");
-let auxiliar = 0;
+        element.style.display = "none";
 
-botao_abrir.addEventListener("click", function(){
+    }
 
-    document.querySelector("#info-menu").classList.add("animation");
-    showEffect(document.querySelector("#info-menu"));
+    botao_abrir.addEventListener("click", function(){
 
-});
+        document.querySelector("#info-menu").classList.add("animation");
+        showEffect(document.querySelector("#info-menu"));
 
-
-botao_fechar.addEventListener("click", function(){
-
-    closeEffect(document.querySelector("#info-menu"));
+    });
 
 
-});
+    botao_fechar.addEventListener("click", function(){
 
-for(let i = 0; i <= expandir_janela.length; i++){
-            
-            expandir_janela[i].onclick = () =>{ 
+        closeEffect(document.querySelector("#info-menu"));
+
+
+    });
+
+    for(let i = 0; i <= expandir_janela.length; i++){
                 
-                expandir_janela[i].classList.add("expandir");
-                expandir_janela[i].classList.add("animations");
+                expandir_janela[i].onmouseover = () =>{ 
+                    
+                    expandir_janela[i].classList.add("expandir");
+                    expandir_janela[i].classList.add("animations");
+                    
+                }
 
-                expandir_janela[i].onclick = () =>{
+                expandir_janela[i].onmouseout = () =>{
 
                     expandir_janela[i].classList.remove("expandir");
                     expandir_janela[i].classList.remove("animations");
-                }   
-            }
-        
-    }
+                } 
+            
+        }
 
 
+})();
